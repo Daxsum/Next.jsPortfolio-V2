33febb1de514feb6547decc0dfc0f5a9fe5c5451
daxsum <kaldax250@gmail.com>
2024-01-14 18:48:27 +0300

    feat:openAI blog

--- data/skills.ts
+++ data/skills.ts
@@ -1,142 +1,173 @@
 const skills = [
-    {
-    id:1,
-    skill: " figma ",
-    logo: "/static/images/skills/figma.jpg",
-    alt:"figma_logo",
-    persent:85,
-    year: "2017-present"    
-    },
-    {
-    id:2,
-    skill: " HTML ",
-    logo: "/static/images/skills/html.jpeg",
-    alt:"html_logo",
-    persent:99,
-    year: "2017-present"    
-    },
-    {
-    id:3,
-    skill: " css ",
-    logo: "/static/images/skills/css.png",
-    alt:"css_logo",
-    persent:99,
-    year: "2017-present"    
-    },
-    {
-    id:4,
-    skill: " javaScript ",
-    logo: "/static/images/skills/js.jpeg",
-    alt:"js_logo",
-    persent:99,
-    year: "2017-present"    
-    },
-    {
-    id:5,
-    skill: " typescript ",
-    logo: "/static/images/skills/typescript.png",
-    alt:"ts_logo",
-    persent:95,
-    year: "2017-present"    
-    },
-    {
-    id:6,
-    skill: " react ",
-    logo: "/static/images/skills/react.jpeg",
-    alt:"react_logo",
-    persent:99,
-    year: "2017-present"    
-    },
-    {
-    id:7,
-    skill: " redux ",
-    logo: "/static/images/skills/redux.webp",
-    alt:"redux_logo",
-    persent:98,
-    year: "2017-present"    
-    },
-    {
-    id:8,
-    skill: " Next.js ",
-    logo: "/static/images/skills/nextjs.webp",
-    alt:"next.js_logo",
-    persent:90,
-    year: "2017-present"    
-    },
-    {
-    id:9,
-    skill: " express ",
-    logo: "/static/images/skills/express.webp",
-    alt:"express_logo",
-    persent:89,
-    year: "2017-present"    
-    },
-    {
-    id:10,
-    skill: " postgreSQL ",
-    logo: "/static/images/skills/PostgreSQL.jpg",
-    alt:"postgreSQL_logo",
-    persent:85,
-    year: "2017-present"    
-    },
-    {
-    id:11,
-    skill: " mongoDB ",
-    logo: "/static/images/skills/MongoDB.jpg",
-    alt:"mongoDB_logo",
-    persent:88,
-    year: "2017-present"    
-    },
-    {
-    id:11,
-    skill: " Three.js ",
-    logo: "/static/images/skills/threejs-geometries-materials.webp",
-    alt:"three.js",
-    persent:88,
-    year: "2023-present"    
-    },
-    {
-    id:12,
-    skill: " Docker ",
-    logo: "/static/images/skills/Docker.webp",
-    alt:"Docker_logo",
-    persent:80,
-    year: "2017-present"    
-    },
-    {
-    id:13,
-    skill: " AWS ",
-    logo: "/static/images/skills/aws.webp",
-    alt:"aws_logo",
-    persent:80,
-    year: "2017-present"    
-    },
-    {
-    id:14,
-    skill: " Digital Ocean ",
-    logo: "/static/images/skills/digitalOcean.png",
-    alt:"DigitalOcean_logo",
-    persent:75,
-    year: "2017-present"    
-    },
-    
-    {
-    id:15,
-    skill: " vercel ",
-    logo: "/static/images/skills/vercel.jpg",
-    alt:"vercel_logo",
-    persent:95,
-    year: "2017-present"    
-    },
-    {
-    id:16,
-    skill: " Netlify ",
-    logo: "/static/images/skills/netlify.jpg",
-    alt:"netlify_logo",
-    persent:95,
-    year: "2017-present"    
-    },
-  ]
-  
-  export default skills;
-  
+  {
+    id: 1,
+    skill: ' figma ',
+    logo: '/static/images/skills/figma.jpg',
+    alt: 'figma_logo',
+    persent: 85,
+    year: '2017-present',
+  },
+  {
+    id: 2,
+    skill: ' HTML ',
+    logo: '/static/images/skills/html.jpeg',
+    alt: 'html_logo',
+    persent: 99,
+    year: '2017-present',
+  },
+  {
+    id: 3,
+    skill: ' css ',
+    logo: '/static/images/skills/css.png',
+    alt: 'css_logo',
+    persent: 99,
+    year: '2017-present',
+  },
+  {
+    id: 4,
+    skill: ' tailwind css ',
+    logo: '/static/images/skills/tailwind.jpeg',
+    alt: 'tailwind_css_logo',
+    persent: 99,
+    year: '2017-present',
+  },
+  {
+    id: 5,
+    skill: ' javaScript ',
+    logo: '/static/images/skills/js.jpeg',
+    alt: 'js_logo',
+    persent: 99,
+    year: '2017-present',
+  },
+  {
+    id: 6,
+    skill: ' typescript ',
+    logo: '/static/images/skills/typescript.png',
+    alt: 'ts_logo',
+    persent: 95,
+    year: '2017-present',
+  },
+  {
+    id: 7,
+    skill: ' python ',
+    logo: '/static/images/skills/python.png',
+    alt: 'python_logo',
+    persent: 99,
+    year: '2017-present',
+  },
+  {
+    id: 8,
+    skill: ' react ',
+    logo: '/static/images/skills/react.jpeg',
+    alt: 'react_logo',
+    persent: 99,
+    year: '2017-present',
+  },
+  {
+    id: 9,
+    skill: ' redux ',
+    logo: '/static/images/skills/redux.webp',
+    alt: 'redux_logo',
+    persent: 98,
+    year: '2017-present',
+  },
+  {
+    id: 10,
+    skill: ' Next.js ',
+    logo: '/static/images/skills/nextjs.webp',
+    alt: 'next.js_logo',
+    persent: 90,
+    year: '2017-present',
+  },
+  {
+    id: 11,
+    skill: ' express ',
+    logo: '/static/images/skills/express.webp',
+    alt: 'express_logo',
+    persent: 89,
+    year: '2017-present',
+  },
+  {
+    id: 12,
+    skill: ' postgreSQL ',
+    logo: '/static/images/skills/PostgreSQL.jpg',
+    alt: 'postgreSQL_logo',
+    persent: 85,
+    year: '2017-present',
+  },
+  {
+    id: 13,
+    skill: ' mongoDB ',
+    logo: '/static/images/skills/MongoDB.jpg',
+    alt: 'mongoDB_logo',
+    persent: 88,
+    year: '2017-present',
+  },
+  {
+    id: 14,
+    skill: ' Three.js ',
+    logo: '/static/images/skills/threejs-geometries-materials.webp',
+    alt: 'three.js',
+    persent: 88,
+    year: '2023-present',
+  },
+  {
+    id: 15,
+    skill: ' Docker ',
+    logo: '/static/images/skills/Docker.webp',
+    alt: 'Docker_logo',
+    persent: 80,
+    year: '2017-present',
+  },
+  {
+    id: 16,
+    skill: ' AWS ',
+    logo: '/static/images/skills/aws.webp',
+    alt: 'aws_logo',
+    persent: 80,
+    year: '2017-present',
+  },
+  {
+    id: 17,
+    skill: ' Nignx ',
+    logo: '/static/images/skills/nginx.jpeg',
+    alt: 'Nginx_logo',
+    persent: 75,
+    year: '2017-present',
+  },
+  {
+    id: 18,
+    skill: ' Apache ',
+    logo: '/static/images/skills/apache.jpeg',
+    alt: 'Apache_logo',
+    persent: 75,
+    year: '2017-present',
+  },
+  {
+    id: 19,
+    skill: ' Digital Ocean ',
+    logo: '/static/images/skills/digitalOcean.png',
+    alt: 'DigitalOcean_logo',
+    persent: 75,
+    year: '2017-present',
+  },
+
+  {
+    id: 20,
+    skill: ' vercel ',
+    logo: '/static/images/skills/vercel.jpg',
+    alt: 'vercel_logo',
+    persent: 95,
+    year: '2017-present',
+  },
+  {
+    id: 21,
+    skill: ' Netlify ',
+    logo: '/static/images/skills/netlify.jpg',
+    alt: 'netlify_logo',
+    persent: 95,
+    year: '2017-present',
+  },
+]
+
+export default skills
